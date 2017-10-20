c4c1b8eb01665da1383abd7ea66a63fe39901a3a
demeiyan_dell <1312480794@qq.com>
2017-10-20 19:22:49 +0800

    common.go

--- common.go
+++ common.go
@@ -1,22 +1,34 @@
 package main
+
 import (
 	"fmt"
-
+	"runtime"
 )
-func split(sum int)(x,y int){
-	x = sum*4/9
-	y = sum - x
-	return
+func sqrt(x ,z float64)float64{
 
+	return z-(z*z-x)/(2*z)
 }
-func swap(x,y string)(string,string){
-	return y,x;
-}
-func main(){
-	var i,j int
-	i = 1
-	j = 2
-	a,b:=swap("hello","world")
-	fmt.Println(a,b)
-	fmt.Println(split(9),i,j)
+
+func main() {
+	z :=1.0
+	for i:=1; i <=10;i++  {
+
+		z=sqrt(2,z)
+		if sqrt(5,z)-z < 0.1e-10 {
+			break
+		}
+		fmt.Println(z)
+	}
+	fmt.Println(z)
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.", os)
+	}
 }
--- test.go
+++ test.go
@@ -1,11 +1,23 @@
 package main
 
 import ("fmt"
-       "time"
+
 )
 
 func main() {
-   fmt.Println("Hello, World!")
-   time  := time.Now()
-   fmt.Println(time)
+/*    fmt.Println("When`s Saturday?")
+    today :=time.Now().Weekday()
+    fmt.Println(time.Monday)
+    switch time.Saturday{
+    case today+0:
+        fmt.Println("Today.")
+    case today +1 :
+        fmt.Println("Tomorrow.")
+    case today+2:
+        fmt.Println("In two days.")
+    default:
+        fmt.Println("Too far away.")
+    }*/
+    defer fmt.Println("world")
+    fmt.Println("hello  ")
 }
